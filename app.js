f76a7afb96a5ac4a876d3635fd28b81e5d8ca44b
Twinkle Goyal <art.twis@gmail.com>
2021-05-01 10:41:10 +0530

    changed app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// First line of code
-// Second line of code
-// Third line of code
+// First line of code..
+// Second line of code..
+// Third line of code..
 
 // adding a chart function in our branch ......
+
+// adding new function to login user via x method
